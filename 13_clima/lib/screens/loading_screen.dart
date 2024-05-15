1d04d7e7abc9565eefbde10dfafb88f9bc4253f3
MJ Habib <mj.habib4@gmail.com>
2024-05-15 17:06:28 +0330

    install GeoLocator package and get device's position

--- 13_clima/lib/screens/loading_screen.dart
+++ 13_clima/lib/screens/loading_screen.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: TextButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: const Text('Get Location'),
         ),
