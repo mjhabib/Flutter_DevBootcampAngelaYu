35b4f868eb547bf9fe616573c629885feed7c230
MJ Habib <mj.habib4@gmail.com>
2024-05-16 18:31:03 +0330

    add openWeatherMap API with Json format

--- 13_clima/lib/screens/loading_screen.dart
+++ 13_clima/lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import '../services/location.dart';
+
+const apiKey = '57dcb0312fe1966e9a2714a55edd9464';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -10,6 +14,9 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? lat;
+  double? long;
+
   @override
   void initState() {
     super.initState();
@@ -19,12 +26,36 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.lat);
-    print(location.long);
+
+    lat = location.lat;
+    long = location.long;
+
+    // getDate();
+  }
+
+  // The API needs a VPN to work!
+  void getDate() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$apiKey'));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temp = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String city = decodedData['name'];
+
+      print('Temp:$temp , ID:$condition , City:$city');
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    getDate();
     return const Scaffold();
   }
 }
