d01e74b54bc8a940b44bdfeec9dc67cbd2de1929
MJ Habib <mj.habib4@gmail.com>
2024-05-18 21:15:46 +0330

    init project flash_chat

---
+++ 15_flash_chat/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '/screens/welcome_screen.dart';
+import '/screens/login_screen.dart';
+import '/screens/registration_screen.dart';
+import '/screens/chat_screen.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
