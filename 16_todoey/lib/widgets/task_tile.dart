905a7cdd289d571d5e0a3ad81c03c5035c2b55e8
MJ Habib <mj.habib4@gmail.com>
2024-05-23 20:07:41 +0330

    add dynamic ListView.builder functionality

--- 16_todoey/lib/widgets/task_tile.dart
+++ 16_todoey/lib/widgets/task_tile.dart
@@ -1,62 +1,29 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatefulWidget {
-  const TaskTile({
-    super.key,
-  });
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final void Function(bool?)? toggleCheckboxState;
 
-  @override
-  State<TaskTile> createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  // global state approach, which not only can have access to this 'TaskTile' but other children like 'Checkboxes' too:
-  bool isChecked = false;
-
-  void checkboxCallback(value) {
-    setState(() {
-      isChecked = value;
-    });
-  }
+  const TaskTile(
+      {super.key,
+      required this.taskTitle,
+      required this.isChecked,
+      required this.toggleCheckboxState});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text(
-          'This is a task!',
-          style: TextStyle(
-              decoration: isChecked ? TextDecoration.lineThrough : null),
-        ),
-        trailing: TaskCheckbox(
-            checkboxState: isChecked, toggleCheckboxState: checkboxCallback));
-  }
-}
-
-// this class is the child of a 'StatefulWidget' that's why we need to mark it as 'Stateless' and can't have any 'setState()' in it
-class TaskCheckbox extends StatelessWidget {
-  final bool checkboxState;
-  final void Function(bool?)? toggleCheckboxState;
-
-  const TaskCheckbox(
-      {super.key,
-      required this.checkboxState,
-      required this.toggleCheckboxState});
-
-  // local state approach, which only can have access to this 'Checkbox' not its parents like 'TaskTiles':
-  // bool? isChecked = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      activeColor: Colors.lightBlueAccent,
-      value: checkboxState,
-      onChanged: toggleCheckboxState,
-      // local state approach:
-      // onChanged: (value) {
-      // setState(() {
-      //   isChecked = value;
-      // });
-      // };
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: toggleCheckboxState,
+      ),
     );
   }
 }
--- 16_todoey/lib/widgets/tasks_list.dart
+++ 16_todoey/lib/widgets/tasks_list.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:todoey/widgets/task_tile.dart';
+import '../widgets/task_tile.dart';
+import '../models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({
     super.key,
   });
 
   @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'buy milk'),
+    Task(name: 'pickup the books'),
+    Task(name: 'have fun!'),
+    Task(name: 'new task')
+  ];
+
+  void checkboxCallback;
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const [TaskTile(), TaskTile()],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+          taskTitle: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          toggleCheckboxState: (value) {
+            setState(() {
+              tasks[index].toggleDone();
+            });
+          },
+        );
+      },
+      itemCount: tasks
+          .length, // the ListView.builder needs to know how many items to create
     );
   }
 }
